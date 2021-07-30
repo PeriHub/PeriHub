8592bc7bb1dc15b7b842dc91f695c85f232f087f
hess_ja <jan-timo.hesse@dlr.de>
2021-07-30 17:37:11 +0200

    Fixed Cluster Acces

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,16 @@
 FROM python:3.7
 
-RUN pip install fastapi uvicorn matplotlib scipy numpy aiofiles pandas
+RUN pip install fastapi uvicorn matplotlib scipy numpy aiofiles pandas paramiko pysftp
+
+RUN mkdir -p /root/.ssh && \
+    chmod 0700 /root/.ssh && \
+    ssh-keyscan 129.247.54.37 > /root/.ssh/known_hosts
+
+COPY id_rsa_shared /root/.ssh/id_rsa
+
+RUN chmod 600 /root/.ssh/id_rsa
+
+RUN echo "Host 129.247.54.37\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
 WORKDIR /app/
 
--- app/modelGeneratorControl.py
+++ app/modelGeneratorControl.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2021 Deutsches Zentrum fuer Luft- und Raumfahrt(DLR, German Aerospace Center) <www.dlr.de>
 
 
+from ntpath import join
 from numpy import string_
 from numpy.lib.shape_base import split
 from GIICmodel.GIICmodel import GIICmodel
@@ -15,11 +16,13 @@ import pandas as pd
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from enum import Enum
 
 import shutil
-
+import paramiko
+import os
 class ModelName(str, Enum):
     GIICmodel = "GIICmodel"
     DCBmodel = "DCBmodel"
@@ -31,10 +34,23 @@ class FileType(str, Enum):
     xml = "xml"
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class ModelControl(object):
 
-    @app.post("/writeModel")
-    def writeModel(ModelName: ModelName, Length: float, Width: float, Height: float, Discretization: float, TwoDimensional: bool, RotatedAngles: bool, Solvertype: SolverType, Filetype: FileType):
+    @app.post("/generateModel")
+    def generateModel(ModelName: ModelName, Length: float, Width: float, Height: float, Discretization: float, TwoDimensional: bool, RotatedAngles: bool, Solvertype: SolverType, Filetype: FileType):
         # L = 152
         # L = 50
         # W = 10
@@ -51,19 +67,26 @@ class ModelControl(object):
             gc = GIICmodel(xend = L, yend = h, zend = W, dx=dx, solvertype = Solvertype, TwoD = TwoDimensional, filetype = Filetype, rot=RotatedAngles)
             model = gc.createModel()
 
-            return {'GIIC': "GIIC-Model has been created", 'dx': dx}
-
         if ModelName==ModelName.DCBmodel:
             dcb = DCBmodel(xend = L, yend = h, zend = W, dx=dx, solvertype = Solvertype, TwoD = TwoDimensional, filetype = Filetype, rot=RotatedAngles)
             model = dcb.createModel()
 
-            return {'DCB': "DCB-Model has been created", 'dx': dx}
+        return {ModelName: ModelName + "-Model has been created", 'dx': dx}
         
         
     @app.get("/viewInputFile")
-    def viewInputFile(ModelName: ModelName):
+    def viewInputFile(ModelName: ModelName, FileType: FileType):
 
-        return FileResponse('./Output/' + ModelName + '/'  + ModelName + '.yaml')
+        return FileResponse('./Output/' + ModelName + '/'  + ModelName + '.' + FileType)
+
+    @app.post("/writeInputFile")
+    def writeInputFile(ModelName: ModelName, InputString: str, FileType: FileType):
+
+        fid = open('./Output/' + ModelName + '/'  + ModelName + '.' + FileType ,'w')
+        fid.write(InputString)
+        fid.close()
+
+        return {ModelName: ModelName + "-InputFile has been written"}
 
     @app.get("/getModel")
     def getModel(ModelName: ModelName):
@@ -74,6 +97,45 @@ class ModelControl(object):
         response.headers["Content-Disposition"] = "attachment; filename=" + ModelName + ".zip"
         # return StreamingResponse(iterfile(), media_type="application/x-zip-compressed")
         return response
+
+    @app.get("/copyModelToCluster")
+    def copyModelToCluster(ModelName: ModelName):
+
+        username='hess_ja'
+        server='129.247.54.37'
+        remotepath = './Peridigm/apiModels/' + ModelName
+        ssh = paramiko.SSHClient() 
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(server, username=username)
+        sftp = ssh.open_sftp()
+        try:
+            sftp.chdir(remotepath)  # Test if remote_path exists
+        except IOError:
+            sftp.mkdir(remotepath)  # Create remote_path
+            sftp.chdir(remotepath)
+        for root, dirs, files in os.walk('./Output/' + ModelName):
+            for name in files:
+                sftp.put(os.path.join(root,name), name)
+
+        sftp.close()
+        ssh.close()
+
+        return {ModelName: ModelName + "-Model has been copied to Cluster"}
+
+    @app.get("/runModel")
+    def runModel(ModelName: ModelName, FileType: FileType, Tasks: int):
+
+        username='hess_ja'
+        server='129.247.54.37'
+        remotepath = './Peridigm/apiModels/' + ModelName
+        ssh = paramiko.SSHClient() 
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(server, username=username)
+        command = 'cd ' + remotepath + ' \n qperidigm -d -c ' + str(Tasks) + ' -O tgz -J ' + ModelName +' -E /home/hess_ja/PeridigmInstall/build/bin/Peridigm '+ ModelName + '.' + FileType
+        ssh.exec_command(command)
+        ssh.close()
+
+        return {ModelName: ModelName + "-Model has been submitted"}
         
     def __init__(self,**kwargs):
         """doc"""
