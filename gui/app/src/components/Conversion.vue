e4506f564e8dd4eb225efeb892adaefb737ff6e6
hess_ja <jan-timo.hesse@dlr.de>
2022-03-09 16:55:56 +0100

    fixed conversion routines

--- gui/app/src/components/Conversion.vue
+++ gui/app/src/components/Conversion.vue
@@ -169,126 +169,136 @@ export default {
       const v = this.constants.poissonsRatio;
       const M = this.constants.pWaveModulus;
       if (K != null) {
-        this.calculated.bulkModulus = K;
+        this.calculated.bulkModulus = +K;
         if (E != null) {
-          this.calculated.youngsModulus = E;
-          this.calculated.lameFirst = (3 * K * (3 * K - E)) / (9 * K - E);
-          this.calculated.shearModulus = (3 * K * E) / (9 * K - E);
-          this.calculated.poissonsRatio = (3 * K - E) / (6 * K);
-          this.calculated.pWaveModulus = (3 * K * (3 * K + E)) / (9 * K - E);
+          this.calculated.youngsModulus = +E;
+          this.calculated.lameFirst = (3 * +K * (3 * +K - +E)) / (9 * +K - +E);
+          this.calculated.shearModulus = (3 * +K * +E) / (9 * +K - +E);
+          this.calculated.poissonsRatio = (3 * +K - +E) / (6 * +K);
+          this.calculated.pWaveModulus =
+            (3 * +K * (3 * +K + +E)) / (9 * +K - +E);
         }
         if (L != null) {
-          this.calculated.youngsModulus = (9 * K * (K - L)) / (3 * K - L);
-          this.calculated.lameFirst = L;
-          this.calculated.shearModulus = (3 * K - L) / 2;
-          this.calculated.poissonsRatio = L / (3 * K - L);
-          this.calculated.pWaveModulus = 3 * K - 2 * L;
+          this.calculated.youngsModulus = (9 * +K * (+K - +L)) / (3 * +K - +L);
+          this.calculated.lameFirst = +L;
+          this.calculated.shearModulus = (3 * +K - +L) / 2;
+          this.calculated.poissonsRatio = +L / (3 * +K - +L);
+          this.calculated.pWaveModulus = 3 * +K - 2 * +L;
         }
         if (G != null) {
-          this.calculated.youngsModulus = (9 * K * G) / (3 * K + G);
-          this.calculated.lameFirst = K - (2 * G) / 3;
-          this.calculated.shearModulus = G;
-          this.calculated.poissonsRatio = (3 * K - 2 * G) / (2 * (3 * K - G));
-          this.calculated.pWaveModulus = K + (4 * G) / 3;
+          this.calculated.youngsModulus = (9 * +K * +G) / (3 * +K + +G);
+          this.calculated.lameFirst = +K - (2 * +G) / 3;
+          this.calculated.shearModulus = +G;
+          this.calculated.poissonsRatio =
+            (3 * +K - 2 * +G) / (2 * (3 * +K - +G));
+          this.calculated.pWaveModulus = +K + (4 * +G) / 3;
         }
         if (v != null) {
-          this.calculated.youngsModulus = 3 * K * (1 - 2 * v);
-          this.calculated.lameFirst = (3 * K * v) / (1 + v);
-          this.calculated.shearModulus = (3 * K * (1 - 2 * v)) / (2 * (1 + v));
-          this.calculated.poissonsRatio = v;
-          this.calculated.pWaveModulus = (3 * K * (1 - v)) / (1 + v);
+          this.calculated.youngsModulus = 3 * +K * (1 - 2 * +v);
+          this.calculated.lameFirst = (3 * +K * +v) / (1 + +v);
+          this.calculated.shearModulus =
+            (3 * +K * (1 - 2 * +v)) / (2 * (1 + +v));
+          this.calculated.poissonsRatio = +v;
+          this.calculated.pWaveModulus = (3 * +K * (1 - +v)) / (1 + +v);
         }
         if (M != null) {
-          this.calculated.youngsModulus = (9 * K * (M - K)) / (3 * K + M);
-          this.calculated.lameFirst = (3 * K - M) / 2;
-          this.calculated.shearModulus = (3 * (M - K)) / 4;
-          this.calculated.poissonsRatio = (3 * K - M) / (3 * K + M);
-          this.calculated.pWaveModulus = M;
+          this.calculated.youngsModulus = (9 * +K * (+M - +K)) / (3 * +K + +M);
+          this.calculated.lameFirst = (3 * +K - +M) / 2;
+          this.calculated.shearModulus = (3 * (+M - +K)) / 4;
+          this.calculated.poissonsRatio = (3 * +K - +M) / (3 * +K + +M);
+          this.calculated.pWaveModulus = +M;
         }
       }
       if (E != null) {
-        this.calculated.youngsModulus = E;
+        this.calculated.youngsModulus = +E;
         if (L != null) {
-          const R = Math.sqrt(Math.pow(E, 2) + 9 * Math.pow(L, 2) + 2 * E * L);
-          this.calculated.bulkModulus = (E + 3 * L + R) / 6;
-          this.calculated.lameFirst = L;
-          this.calculated.shearModulus = (E - 3 * L + R) / 4;
-          this.calculated.poissonsRatio = (2 * L) / (E + L + R);
-          this.calculated.pWaveModulus = (E - L + R) / 2;
+          const R = Math.sqrt(
+            Math.pow(+E, 2) + 9 * Math.pow(+L, 2) + 2 * +E * +L
+          );
+          this.calculated.bulkModulus = (+E + 3 * +L + +R) / 6;
+          this.calculated.lameFirst = +L;
+          this.calculated.shearModulus = (+E - 3 * +L + +R) / 4;
+          this.calculated.poissonsRatio = (2 * +L) / (+E + +L + +R);
+          this.calculated.pWaveModulus = (+E - +L + +R) / 2;
         }
         if (G != null) {
-          this.calculated.bulkModulus = (E * G) / (3 * (3 * G - E));
-          this.calculated.lameFirst = (G * (E - 2 * G)) / (3 * G - E);
-          this.calculated.shearModulus = G;
-          this.calculated.poissonsRatio = E / (2 * G) - 1;
-          this.calculated.pWaveModulus = (G * (4 * G - E)) / (3 * G - E);
+          this.calculated.bulkModulus = (+E * +G) / (3 * (3 * +G - +E));
+          this.calculated.lameFirst = (+G * (+E - 2 * +G)) / (3 * +G - +E);
+          this.calculated.shearModulus = +G;
+          this.calculated.poissonsRatio = +E / (2 * +G) - 1;
+          this.calculated.pWaveModulus = (+G * (4 * +G - +E)) / (3 * +G - +E);
         }
         if (v != null) {
-          this.calculated.bulkModulus = E / (3 * (1 - 2 * v));
-          this.calculated.lameFirst = (E * v) / ((1 + v) * (1 - 2 * v));
-          this.calculated.shearModulus = E / (2 * (1 + v));
-          this.calculated.poissonsRatio = v;
+          this.calculated.bulkModulus = +E / (3 * (1 - 2 * +v));
+          this.calculated.lameFirst = (+E * +v) / ((1 + +v) * (1 - 2 * +v));
+          this.calculated.shearModulus = +E / (2 * (1.0 + +v));
+          this.calculated.poissonsRatio = +v;
           this.calculated.pWaveModulus =
-            (E * (1 - v)) / ((1 + v) * (1 - 2 * v));
+            (+E * (1 - +v)) / ((1 + +v) * (1 - 2 * +v));
         }
         if (M != null) {
-          const S = Math.sqrt(Math.pow(E, 2) + 9 * Math.pow(M, 2) - 10 * E * M);
-          this.calculated.bulkModulus = (3 * M - E + S) / 6;
-          this.calculated.lameFirst = (M - E + S) / 4;
-          this.calculated.shearModulus = (3 * M + E - S) / 8;
-          this.calculated.poissonsRatio = (E - M + S) / (4 * M);
-          this.calculated.pWaveModulus = M;
+          const S = Math.sqrt(
+            Math.pow(+E, 2) + 9 * Math.pow(+M, 2) - 10 * +E * +M
+          );
+          this.calculated.bulkModulus = (3 * +M - +E + +S) / 6;
+          this.calculated.lameFirst = (+M - +E + +S) / 4;
+          this.calculated.shearModulus = (3 * +M + +E - +S) / 8;
+          this.calculated.poissonsRatio = (+E - +M + +S) / (4 * +M);
+          this.calculated.pWaveModulus = +M;
         }
       }
       if (L != null) {
-        this.calculated.lameFirst = L;
+        this.calculated.lameFirst = +L;
         if (G != null) {
-          this.calculated.bulkModulus = L + (2 * G) / 3;
-          this.calculated.youngsModulus = (G * (3 * L + 2 * G)) / (L * G);
-          this.calculated.shearModulus = G;
-          this.calculated.poissonsRatio = L / (2 * (L + G));
-          this.calculated.pWaveModulus = L + 2 * G;
+          this.calculated.bulkModulus = +L + (2 * +G) / 3;
+          this.calculated.youngsModulus = (+G * (3 * +L + 2 * +G)) / (+L * +G);
+          this.calculated.shearModulus = +G;
+          this.calculated.poissonsRatio = +L / (2 * (+L + +G));
+          this.calculated.pWaveModulus = +L + 2 * +G;
         }
         if (v != null) {
-          this.calculated.bulkModulus = (L * (1 + v)) / (3 * v);
-          this.calculated.youngsModulus = (L * (1 + v) * (1 - 2 * v)) / v;
-          this.calculated.shearModulus = (L * (1 - 2 * v)) / (2 * v);
-          this.calculated.poissonsRatio = v;
-          this.calculated.pWaveModulus = (L * (1 - v)) / v;
+          this.calculated.bulkModulus = (+L * (1 + v)) / (3 * +v);
+          this.calculated.youngsModulus = (+L * (1 + +v) * (1 - 2 * +v)) / +v;
+          this.calculated.shearModulus = (+L * (1 - 2 * +v)) / (2 * +v);
+          this.calculated.poissonsRatio = +v;
+          this.calculated.pWaveModulus = (+L * (1 - +v)) / +v;
         }
         if (M != null) {
-          this.calculated.bulkModulus = (M + 2 * L) / 3;
-          this.calculated.youngsModulus = ((M - L) * (M + 2 * L)) / (M + L);
-          this.calculated.shearModulus = (M - L) / 2;
-          this.calculated.poissonsRatio = L / (M + L);
-          this.calculated.pWaveModulus = M;
+          this.calculated.bulkModulus = (+M + 2 * +L) / 3;
+          this.calculated.youngsModulus =
+            ((+M - +L) * (+M + 2 * +L)) / (+M + +L);
+          this.calculated.shearModulus = (+M - +L) / 2;
+          this.calculated.poissonsRatio = +L / (+M + +L);
+          this.calculated.pWaveModulus = +M;
         }
       }
       if (G != null) {
-        this.calculated.shearModulus = G;
+        this.calculated.shearModulus = +G;
         if (v != null) {
-          this.calculated.bulkModulus = (2 * G * (1 + v)) / (3 * (1 - 2 * v));
-          this.calculated.youngsModulus = 2 * G * (1 + v);
-          this.calculated.lameFirst = (2 * G * v) / (1 - 2 * v);
-          this.calculated.poissonsRatio = v;
-          this.calculated.pWaveModulus = (2 * G * (1 - v)) / (1 - 2 * v);
+          this.calculated.bulkModulus =
+            (2 * +G * (1 + +v)) / (3 * (1 - 2 * +v));
+          this.calculated.youngsModulus = 2 * +G * (1 + +v);
+          this.calculated.lameFirst = (2 * +G * +v) / (1 - 2 * +v);
+          this.calculated.poissonsRatio = +v;
+          this.calculated.pWaveModulus = (2 * +G * (1 - +v)) / (1 - 2 * +v);
         }
         if (M != null) {
-          this.calculated.bulkModulus = M - (4 * G) / 3;
-          this.calculated.youngsModulus = (G * (3 * M - 4 * G)) / (M - G);
-          this.calculated.lameFirst = M - 2 * G;
-          this.calculated.poissonsRatio = (M - 2 * G) / (2 * M - 2 * G);
-          this.calculated.pWaveModulus = M;
+          this.calculated.bulkModulus = +M - (4 * +G) / 3;
+          this.calculated.youngsModulus = (+G * (3 * +M - 4 * +G)) / (+M - +G);
+          this.calculated.lameFirst = +M - 2 * +G;
+          this.calculated.poissonsRatio = (+M - 2 * +G) / (2 * +M - 2 * +G);
+          this.calculated.pWaveModulus = +M;
         }
       }
       if (v != null) {
-        this.calculated.poissonsRatio = v;
+        this.calculated.poissonsRatio = +v;
         if (M != null) {
-          this.calculated.bulkModulus = (M * (1 + v)) / (3 * (1 - v));
-          this.calculated.youngsModulus = (M * (1 + v) * (1 - 2 * v)) / (1 - v);
-          this.calculated.lameFirst = (M * v) / (1 - v);
-          this.calculated.shearModulus = (M * (1 - 2 * v)) / (2 * (1 - v));
-          this.calculated.pWaveModulus = M;
+          this.calculated.bulkModulus = (+M * (1 + +v)) / (3 * (1 - +v));
+          this.calculated.youngsModulus =
+            (+M * (1 + +v) * (1 - 2 * +v)) / (1 - +v);
+          this.calculated.lameFirst = (+M * +v) / (1 - +v);
+          this.calculated.shearModulus = (+M * (1 - 2 * +v)) / (2 * (1 - +v));
+          this.calculated.pWaveModulus = +M;
         }
       }
       this.calculated.bulkModulus = Number(
