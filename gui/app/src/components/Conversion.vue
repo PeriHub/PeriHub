eebfcf474d8f0c5390e6940a1b4c0dc27e129cb2
hess_ja <jan-timo.hesse@dlr.de>
2022-05-04 10:08:08 +0200

    added amplitude generator

--- gui/app/src/components/Conversion.vue
+++ gui/app/src/components/Conversion.vue
@@ -119,6 +119,85 @@
           </v-card>
         </v-row>
       </v-container>
+      <v-container max-width="1100">
+        <h1>Amplitude Generator</h1>
+        <splitpanes style="height: 100%">
+          <pane min-size="10" size="30" style="height: 100%">
+            <v-card width="400px">
+              <v-col class="textfield-col">
+                <v-text-field
+                  v-model="amplitude.max"
+                  label="Max"
+                  outlined
+                  type="number"
+                ></v-text-field>
+              </v-col>
+              <v-col class="textfield-col" v-show="amplitude.type == 'Type 1'">
+                <v-text-field
+                  v-model="amplitude.min"
+                  label="Min"
+                  outlined
+                  type="number"
+                ></v-text-field>
+              </v-col>
+              <v-col class="textfield-col">
+                <v-text-field
+                  v-model="amplitude.frequency"
+                  label="Frequency"
+                  outlined
+                  type="number"
+                ></v-text-field>
+              </v-col>
+              <v-col class="textfield-col">
+                <v-text-field
+                  v-model="amplitude.end"
+                  label="Time"
+                  outlined
+                  type="number"
+                ></v-text-field>
+              </v-col>
+              <v-col class="textfield-col">
+                <v-select
+                  :items="amplitudeTypes"
+                  v-model="amplitude.type"
+                  label="Type of Amplitude"
+                  outlined
+                ></v-select>
+              </v-col>
+            </v-card>
+          </pane>
+
+          <pane min-size="30" size="70" style="height: 100%">
+            <v-tabs color="deep-purple accent-4" left max-width="1100">
+              <v-tab>Plotly</v-tab>
+              <v-tab>Output</v-tab>
+              <v-tab-item>
+                <v-card>
+                  <plotly
+                    :data="plotData"
+                    :layout="plotLayout"
+                    :options="plotOptions"
+                    :display-mode-bar="true"
+                    :autoResize="true"
+                    :scrollZoom="true"
+                  >
+                  </plotly>
+                </v-card>
+              </v-tab-item>
+              <v-tab-item>
+                <v-card>
+                  <prism-editor
+                    class="my-editor"
+                    v-model="valueOutput"
+                    :highlight="highlighter"
+                    line-numbers
+                  ></prism-editor>
+                </v-card>
+              </v-tab-item>
+            </v-tabs>
+          </pane>
+        </splitpanes>
+      </v-container>
     </div>
   </v-container>
 </template>
@@ -126,11 +205,27 @@
 <script>
 import axios from "axios";
 import bibtexParse from "bibtex-parse-js";
+import { Plotly } from "vue-plotly";
 // import bibFile from '../static/test.bib'
+import { PrismEditor } from "vue-prism-editor";
+import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism-tomorrow.css"; // import syntax highlighting styles
+
+import { Splitpanes, Pane } from "splitpanes";
+import "splitpanes/dist/splitpanes.css";
 
 export default {
   components: {
+    PrismEditor,
+    Splitpanes,
+    Pane,
     bibtexParse,
+    Plotly,
   },
   data() {
     return {
@@ -158,9 +253,60 @@ export default {
         pWaveModulus: "P-wave modulus (M)",
         lameFirst: "Lamé's first parameter (m)",
       },
+      amplitudeTypes: ["Type 1", "Type 2"],
+      amplitude: {
+        max: 10,
+        min: 2,
+        frequency: 5,
+        end: 5,
+        type: "Type 1",
+      },
+      valueOutput: "A*sin(B*(t-C))+D",
+      plotData: [
+        {
+          name: "Load",
+          x: [1, 2, 3, 4],
+          y: [10, 15, 20, 17],
+          type: "scatter",
+        },
+      ],
+      plotLayout: {
+        // title: 'this.model.modelNameSelected',
+        showlegend: true,
+        // margin: { t: 50 },
+        hovermode: "compare",
+        bargap: 0,
+        xaxis: {
+          showgrid: true,
+          zeroline: true,
+          color: "white",
+        },
+        yaxis: {
+          showgrid: true,
+          zeroline: true,
+          color: "white",
+        },
+        plot_bgcolor: "#2D2D2D",
+        paper_bgcolor: "#2D2D2D",
+        font: {
+          color: "white",
+        },
+        modebar: {
+          color: "white",
+          // color: "#6E6E6E"
+        },
+      },
+      plotOptions: {
+        scrollZoom: true,
+        setBackground: "black",
+      },
     };
   },
+  props: ["plotid"],
   methods: {
+    highlighter(code) {
+      return highlight(code, languages.js); // languages.<insert language> to return html with markup
+    },
     convert() {
       const K = this.constants.bulkModulus;
       const E = this.constants.youngsModulus;
@@ -325,6 +471,173 @@ export default {
       this.calculated.poissonsRatio = null;
       this.calculated.pWaveModulus = null;
     },
+    plot() {
+      this.plotData[0].x = [];
+      this.plotData[0].y = [];
+      var n = 1000;
+      var max = parseFloat(this.amplitude.max);
+      var min = parseFloat(this.amplitude.min);
+      var frequency = parseFloat(this.amplitude.frequency);
+      var end = parseFloat(this.amplitude.end);
+      for (var i = 0; i < n; i++) {
+        var t = (i / (n - 1)) * end;
+        this.plotData[0].x[i] = t;
+        for (var j = 0; j < frequency; j++) {
+          if (j == 0) {
+            if (t <= (1 / frequency) * end) {
+              this.plotData[0].y[i] = (t / ((1 / frequency) * end)) * max;
+              break;
+            }
+          } else if (j % 2 != 0) {
+            if (
+              ((j / frequency) * end < t) &
+              (t <= ((j + 1) / frequency) * end)
+            ) {
+              this.plotData[0].y[i] =
+                max -
+                ((t - (j / frequency) * end) / ((1 / frequency) * end)) *
+                  (max - min);
+              break;
+            }
+          } else if (j % 2 == 0) {
+            if (
+              ((j / frequency) * end < t) &
+              (t <= ((j + 1) / frequency) * end)
+            ) {
+              this.plotData[0].y[i] =
+                min +
+                ((t - (j / frequency) * end) / ((1 / frequency) * end)) *
+                  (max - min);
+              break;
+            }
+          }
+        }
+      }
+      this.valueOutput =
+        "double max = " +
+        this.amplitude.max.toString() +
+        ";\n" +
+        "double min = " +
+        this.amplitude.min.toString() +
+        ";\n" +
+        "double frequency = " +
+        this.amplitude.frequency.toString() +
+        ";\n" +
+        "double end = " +
+        this.amplitude.end.toString() +
+        ";\n";
+      for (var j = 0; j < this.amplitude.frequency; j++) {
+        if (j == 0) {
+          this.valueOutput +=
+            "if (t <= 1 / frequency *end) \\{\n" +
+            "value = (t / ((1 / frequency) * end)) * max;\n" +
+            "\\}\n";
+        } else if (j % 2 != 0) {
+          this.valueOutput +=
+            "if ((" +
+            j.toString() +
+            " / frequency *end < t) && (t <= (" +
+            j.toString() +
+            " + 1) / frequency *end)) \\{\n" +
+            "value = max - ((t - (" +
+            j.toString() +
+            " / frequency) * end) / ((1 / frequency) * end)) * (max - min);\n" +
+            "\\}\n";
+        } else if (j % 2 == 0) {
+          this.valueOutput +=
+            "if ((" +
+            j.toString() +
+            " / frequency *end < t) && (t <= (" +
+            j.toString() +
+            " + 1) / frequency *end)) \\{\n" +
+            "value = min + ((t - (" +
+            j.toString() +
+            " / frequency) * end) / ((1 / frequency) * end)) * (max - min);\n" +
+            "\\}\n";
+        }
+      }
+    },
+    plot2() {
+      this.plotData[0].x = [];
+      this.plotData[0].y = [];
+      var n = 1000;
+      var max = parseFloat(this.amplitude.max);
+      var frequency = parseFloat(this.amplitude.frequency);
+      var end = parseFloat(this.amplitude.end);
+      for (var i = 0; i < n; i++) {
+        var t = (i / (n - 1)) * end;
+        this.plotData[0].x[i] = t;
+        for (var j = 0; j < frequency; j++) {
+          if (j == 0) {
+            if (t <= (1 / frequency) * end) {
+              this.plotData[0].y[i] = (t / end) * max * 2;
+              break;
+            }
+          } else if (j % 2 != 0) {
+            if (
+              ((j / frequency) * end < t) &
+              (t <= ((j + 1) / frequency) * end)
+            ) {
+              this.plotData[0].y[i] =
+                (j / frequency) * max * 2 - ((j - 1) / frequency) * max;
+              break;
+            }
+          } else if (j % 2 == 0) {
+            if (
+              ((j / frequency) * end < t) &
+              (t <= ((j + 1) / frequency) * end)
+            ) {
+              this.plotData[0].y[i] =
+                ((t - ((j / 2) * end) / frequency) / end) * max * 2;
+              break;
+            }
+          }
+        }
+      }
+      // this.plotData[0].x = time.split(",");
+      this.valueOutput =
+        "double max = " +
+        this.amplitude.max.toString() +
+        ";\n" +
+        "double frequency = " +
+        this.amplitude.frequency.toString() +
+        ";\n" +
+        "double end = " +
+        this.amplitude.end.toString() +
+        ";\n";
+      for (var j = 0; j < this.amplitude.frequency; j++) {
+        if (j == 0) {
+          this.valueOutput +=
+            "if (t <= 1 / frequency *end) \\{\n" +
+            "value = (t / end) * max * 2;\n" +
+            "\\}\n";
+        } else if (j % 2 != 0) {
+          this.valueOutput +=
+            "if ((" +
+            j.toString() +
+            " / frequency *end < t) && (t <= (" +
+            j.toString() +
+            " + 1) / frequency *end)) \\{\n" +
+            "value = (" +
+            j.toString() +
+            " / frequency) * max * 2 - ((" +
+            j.toString() +
+            " - 1) / frequency) * max;\n" +
+            "\\}\n";
+        } else if (j % 2 == 0) {
+          this.valueOutput +=
+            "if ((" +
+            j.toString() +
+            " / frequency *end < t) && (t <= (" +
+            j.toString() +
+            " + 1) / frequency *end)) \\{\n" +
+            "value = ((t - ((" +
+            j.toString() +
+            " / 2) * end) / frequency) / end) * max * 2;\n" +
+            "\\}\n";
+        }
+      }
+    },
     copyText(id) {
       let input = document.getElementById(id);
       input.select();
@@ -341,6 +654,7 @@ export default {
   mounted() {
     // console.log("mounted")
     this.getCurrentData();
+    this.plot();
   },
   watch: {
     constants: {
@@ -361,6 +675,19 @@ export default {
           this.resetResult();
         }
         localStorage.setItem("constants", JSON.stringify(this.constants));
+      },
+      deep: true,
+    },
+    amplitude: {
+      handler() {
+        console.log("amplitude changed!");
+        localStorage.setItem("model", JSON.stringify(this.model));
+        if (this.amplitude.type == "Type 1") {
+          this.plot();
+        } else {
+          this.plot2();
+        }
+        localStorage.setItem("amplitude", JSON.stringify(this.amplitude));
       },
       deep: true,
     },
