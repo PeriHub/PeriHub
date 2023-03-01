898d6cf18410d412c60f3d442a64bf6f5ee5f469
hess_ja <jan-timo.hesse@dlr.de>
2023-03-01 14:55:15 +0100

    fixed api url

--- quasar-gui/app/src/boot/axios.js
+++ quasar-gui/app/src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "http://localhost:6020/" });
+const api = axios.create({ baseURL: process.env.API });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
