d619f87af6d3ec0758f26556ce5217604cbb0d60
hess_ja <jan-timo.hesse@dlr.de>
2023-05-31 14:31:58 +0200

    added intel module to sbatch

--- api/app/support/sbatch_creator.py
+++ api/app/support/sbatch_creator.py
@@ -66,6 +66,7 @@ class SbatchCreator:
         string += "module load GCCcore/10.2.0" + "\n"
         string += "module load CMake/3.18.4" + "\n"
         string += "module load Eigen" + "\n"
+        string += "module load intel" + "\n"
         string += "export PATH=$PATH:/home/" + self.user + "/software/trilinos/bin" + "\n"
 
         string += (
