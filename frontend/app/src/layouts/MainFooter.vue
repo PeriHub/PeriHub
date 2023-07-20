ee5da6da4049696f94912d81b8f58f9f4ecd5ada
hess_ja <jan-timo.hesse@dlr.de>
2023-07-20 14:51:19 +0200

    added copyright to footer

--- frontend/app/src/layouts/MainFooter.vue
+++ frontend/app/src/layouts/MainFooter.vue
@@ -5,38 +5,38 @@ SPDX-License-Identifier: Apache-2.0
 -->
 
 <template>
-    <q-footer elevated color="primary">
-      <q-toolbar>
-        <div style="text-align: center; width:100%">
-          <div>
-            <q-btn flat text-color="white" label="ABOUT US" href="https://www.dlr.de/fa"></q-btn>
-            <q-btn flat text-color="white" label="BLOG" href="https://leichtbau.dlr.de/"></q-btn>
-            <q-btn flat text-color="white" label="CONTACT US" href="mailto:Jan-Timo.Hesse@dlr.de"></q-btn>
-          </div>
-          <div>
-            {{ new Date().getFullYear() }} — <strong>PeriHub</strong> | Jan-Timo
-            Hesse | Christian Willberg | Falk Heinecke
-          </div>
+  <q-footer elevated color="primary">
+    <q-toolbar>
+      <div style="text-align: center; width:100%">
+        <div>
+          <q-btn flat text-color="white" label="ABOUT US" href="https://www.dlr.de/fa"></q-btn>
+          <q-btn flat text-color="white" label="BLOG" href="https://leichtbau.dlr.de/"></q-btn>
+          <q-btn flat text-color="white" label="CONTACT US" href="mailto:Jan-Timo.Hesse@dlr.de"></q-btn>
         </div>
-      </q-toolbar>
-    </q-footer>
+        <div>
+          © {{ new Date().getFullYear() }} German Aerospace Center (DLR) — <strong>PeriHub</strong> | Jan-Timo
+          Hesse | Christian Willberg | Falk Heinecke
+        </div>
+      </div>
+    </q-toolbar>
+  </q-footer>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import { useDefaultStore } from 'stores/default-store';
 export default defineComponent({
-    name: "MainFooter",
-    setup() {
-        const store = useDefaultStore();
-        return {
-        store,
-        }
-    },
-    data() {
-        return {
-        };
-    },
-    })
+  name: "MainFooter",
+  setup() {
+    const store = useDefaultStore();
+    return {
+      store,
+    }
+  },
+  data() {
+    return {
+    };
+  },
+})
 </script>
 <style></style>
