4d6d9659571177aedd3c9cefb4666ea40b22c00f
Jan-Timo Hesse <jan-timo.hesse@dlr.de>
2025-07-31 16:24:21 +0200

    fixed rules

--- frontend/app/src/assets/rules.js
+++ frontend/app/src/assets/rules.js
@@ -3,25 +3,25 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export default {
-  rules: {
-    required: (value) => !!value || value == 0 || "Required",
-    name: (value) => {
-      const pattern = /^[A-Za-z0-9_]{1,15}/;
-      return pattern.test(value) || "Invalid name";
-    },
-    posFloat: (value) => {
+  required: [(val) => !!val || 'Field is required'],
+  name: (value) => {
+    const pattern = /^[A-Za-z0-9_]{1,15}/;
+    return pattern.test(value) || 'Invalid name';
+  },
+  posFloat: (value) => {
+    if (value != null) {
       const pattern = /^((?!0)|(?=0+\.))(\d*\.)?\d+(e[-]\d+)?$|^0$/;
-      return pattern.test(value) || "Invalid number";
-    },
-    float: (value) => {
-      if (value != null) {
-        const pattern = /^((?!0)|[-]|(?=0+\.))(\d*\.)?\d+(e[-]\d+)?$|^0$/;
-        return pattern.test(value) || "Invalid number";
-      } else return true;
-    },
-    int: (value) => {
-      const pattern = /^[-]{0,1}(?<!\.)\d+(?!\.)$/;
-      return pattern.test(value) || "Invalid number";
-    },
+      return pattern.test(value) || 'Invalid number';
+    } else return true;
+  },
+  float: (value) => {
+    if (value != null) {
+      const pattern = /^((?!0)|[-]|(?=0+\.))(\d*\.)?\d+(e[-]\d+)?$|^0$/;
+      return pattern.test(value) || 'Invalid number';
+    } else return true;
+  },
+  int: (value) => {
+    const pattern = /^[-]{0,1}(?<!\.)\d+(?!\.)$/;
+    return pattern.test(value) || 'Invalid number';
   },
 };
