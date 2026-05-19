9ed2420343c1551b620f4c5c20aa0f048f76d127
Jan-Timo <jan-timo.hesse@dlr.de>
2026-05-19 10:44:33 +0200

    prism fix

--- frontend/app/src/pages/Models.vue
+++ frontend/app/src/pages/Models.vue
@@ -101,6 +101,7 @@ SPDX-License-Identifier: Apache-2.0
 <script lang="ts">
 
 import { useDefaultStore } from 'src/stores/default-store';
+import Prism from "prismjs";
 import { highlight, languages } from 'prismjs/components/prism-core';
 import { PrismEditor } from 'vue-prism-editor';
 import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhere
